cd9296456a8f8005057214efe8b56cf560e427b2
NomanSanaullah <99878827+noman2233@users.noreply.github.com>
2026-03-23 17:39:30 +0500

    add updated code

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ const page = () => {
   return (
     <div>
       <Navbar/>
+      <Contact/>
       <Footer/>
     </div>
   )
---
+++ components/inputComponent/InputComponent.jsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import styles from "../../styles/inputComponent.module.css";
+
+const  InputComponent=({
+  label,
+  placeholder,
+  editable = true,
+  type = "text",
+  value,
+  onChangeText,
+  onBlur,
+  isMultiline = false,
+  error,
+}) =>{
+  return (
+    <div className={styles.inputContainer}>
+ 
+      <div className={styles.inputWrapper}>
+        {isMultiline ? (
+          <textarea
+            placeholder={placeholder}
+            value={value}
+            onChange={(e) => onChangeText?.(e.target.value)}
+            onBlur={(e) => onBlur?.(e.target.value)}
+            readOnly={!editable}
+            className={styles.message_area}
+            rows={8}
+          />
+        ) : (
+          <input
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={(e) => onChangeText?.(e.target.value)}
+            onBlur={(e) => onBlur?.(e.target.value)}
+            // readOnly={!editable}
+            className={styles.contact_input}
+            // autoCapitalize="none"
+            required
+          />
+        )}
+      </div>
+
+      {error && <p className={styles.errorText}>{error}</p>}
+    </div>
+  );
+}
+
+
+export default InputComponent
